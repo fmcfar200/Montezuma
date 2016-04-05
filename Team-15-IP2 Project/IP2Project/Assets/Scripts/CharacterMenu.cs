1607bf789b54406b1d647bb515b36c2c42e1d273
SMCCOL202 <smccol202@caledonian.ac.uk>
2016-04-05 23:04:41 +0100

    Pre Beta

--- Team-15-IP2 Project/IP2Project/Assets/Scripts/CharacterMenu.cs
+++ Team-15-IP2 Project/IP2Project/Assets/Scripts/CharacterMenu.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CharacterMenu : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
 
---
+++ Team-15-IP2 Project/IP2Project/Assets/Scripts/WinnerSprite.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinnerSprite : MonoBehaviour {
+
+	public Sprite yellow;
+	public Sprite red;
+	public Sprite blue;
+	public Sprite green;
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GameObject g = GameObject.Find ("ScoreObject");
+		if (g != null) 
+		{
+			WinScript win = g.GetComponent<WinScript> ();
+			
+			if (win.winningPlayer == 1)
+			{
+				spriteRenderer.sprite = yellow;
+			}
+			else if(win.winningPlayer == 2)
+			{
+				spriteRenderer.sprite = red;
+			}
+			else if(win.winningPlayer == 3)
+			{
+				spriteRenderer.sprite = green;
+			}
+			else if(win.winningPlayer == 4)
+			{
+				spriteRenderer.sprite = blue;
+			}
+
+		}
+	
+	}
+}
