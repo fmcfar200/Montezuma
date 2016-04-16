53bc20df0893055d6e1e9c73ca0d5f2d74621003
fmcfar200 <fmcfar200@caledonian.ac.uk>
2016-04-17 00:46:45 +0100

    Post Beta
    Added UI for pickups. Kinda weird on how they stack the powers but its
    working okay for now. K bye

---
+++ Team-15-IP2 Project/IP2Project/Assets/PickUpUIScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PickUpUIScript : MonoBehaviour {
+
+
+	public Image spriteUI;
+	public Sprite freeze;
+	public Sprite wind;
+	public Sprite shield;
+	public Sprite speed;
+
+	PlayerAttackScript playerAttackScript;
+	// Use this for initialization
+	void Start () 
+	{
+		playerAttackScript = GetComponent<PlayerAttackScript> ();
+		spriteUI.gameObject.SetActive (false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (playerAttackScript.freezePowerReady == true) {
+			spriteUI.gameObject.SetActive (true);
+			spriteUI.sprite = freeze;
+		} else if (playerAttackScript.windPowerReady == true) {
+			spriteUI.gameObject.SetActive (true);
+			spriteUI.sprite = wind;
+		} else if (playerAttackScript.shieldPowerReady == true) {
+			spriteUI.gameObject.SetActive (true);
+			spriteUI.sprite = shield;
+		} else if (playerAttackScript.speedPowerReady == true) {
+			spriteUI.gameObject.SetActive (true);
+			spriteUI.sprite = speed;
+		} else {
+			spriteUI.gameObject.SetActive (false);
+		}
+	}
+}
