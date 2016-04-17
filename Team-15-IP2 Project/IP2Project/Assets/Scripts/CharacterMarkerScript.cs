780ce9157c12b1816e88c3ef9ac550c00646ce0d
SMCCOL202 <smccol202@caledonian.ac.uk>
2016-04-17 22:39:32 +0100

    Post Beta
    Made it so the markers above player heads will be the correct colour

---
+++ Team-15-IP2 Project/IP2Project/Assets/Scripts/CharacterMarkerScript.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMarkerScript : MonoBehaviour {
+
+	public GameObject marker;
+
+	public Sprite red;
+	public Sprite yellow;
+	public Sprite green;
+	public Sprite blue;
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer spriteRenderer = marker.GetComponent<SpriteRenderer> ();
+
+		if (this.gameObject.name == "Player1") 
+		{
+			GameObject g = GameObject.Find("Player1Data");
+			
+			if(g != null)
+			{
+				
+				DataScript d = g.GetComponent<DataScript>();
+				
+				if(d.playerSpriteNumber == 1)
+				{
+					spriteRenderer.sprite = blue;
+
+					
+				}
+				else if(d.playerSpriteNumber == 2)
+				{
+					spriteRenderer.sprite = green;
+				
+					
+				}
+				else if(d.playerSpriteNumber == 3)
+				{
+					spriteRenderer.sprite = yellow;
+
+				}
+				else if(d.playerSpriteNumber == 4)
+				{
+					spriteRenderer.sprite = red;
+
+				}
+			}
+		}
+		if (this.gameObject.name == "Player2") 
+		{
+			GameObject g = GameObject.Find("Player2Data");
+			
+			if(g != null)
+			{
+				
+				DataScript d = g.GetComponent<DataScript>();
+				
+				if(d.playerSpriteNumber == 1)
+				{
+					spriteRenderer.sprite = blue;
+					
+					
+				}
+				else if(d.playerSpriteNumber == 2)
+				{
+					spriteRenderer.sprite = green;
+					
+					
+				}
+				else if(d.playerSpriteNumber == 3)
+				{
+					spriteRenderer.sprite = yellow;
+					
+				}
+				else if(d.playerSpriteNumber == 4)
+				{
+					spriteRenderer.sprite = red;
+					
+				}
+			}
+		}
+		if (this.gameObject.name == "Player3") 
+		{
+			GameObject g = GameObject.Find("Player3Data");
+			
+			if(g != null)
+			{
+				
+				DataScript d = g.GetComponent<DataScript>();
+				
+				if(d.playerSpriteNumber == 1)
+				{
+					spriteRenderer.sprite = blue;
+					
+					
+				}
+				else if(d.playerSpriteNumber == 2)
+				{
+					spriteRenderer.sprite = green;
+					
+					
+				}
+				else if(d.playerSpriteNumber == 3)
+				{
+					spriteRenderer.sprite = yellow;
+					
+				}
+				else if(d.playerSpriteNumber == 4)
+				{
+					spriteRenderer.sprite = red;
+					
+				}
+			}
+		}
+		if (this.gameObject.name == "Player4") 
+		{
+			GameObject g = GameObject.Find("Player4Data");
+			
+			if(g != null)
+			{
+				
+				DataScript d = g.GetComponent<DataScript>();
+				
+				if(d.playerSpriteNumber == 1)
+				{
+					spriteRenderer.sprite = blue;
+					
+					
+				}
+				else if(d.playerSpriteNumber == 2)
+				{
+					spriteRenderer.sprite = green;
+					
+					
+				}
+				else if(d.playerSpriteNumber == 3)
+				{
+					spriteRenderer.sprite = yellow;
+					
+				}
+				else if(d.playerSpriteNumber == 4)
+				{
+					spriteRenderer.sprite = red;
+					
+				}
+			}
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
